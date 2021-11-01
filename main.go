3b583ff13d6e601ee3746b6a3e63be4693d4dae6
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-11-01 10:06:53 +0100

    :bug: Fixes reading only one line

--- main.go
+++ main.go
@@ -10,15 +10,20 @@ import (
 
 func main() {
 	var reader = bufio.NewReader(os.Stdin)
-	message, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Printf("Error while reading STDIN\n")
-		os.Exit(1)
+	message := ""
+
+	for {
+		char, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+
+		message += string(char)
 	}
 
 	message = strings.TrimSuffix(message, "\n")
 
-	err = clipboard.WriteAll(message)
+	err := clipboard.WriteAll(message)
 	if err != nil {
 		fmt.Printf("Error while writing to clipboard\n")
 		os.Exit(1)
